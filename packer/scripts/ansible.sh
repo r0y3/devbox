4a475dcb31b8cba7723f28f3bcf24ad023697c85
Roy Evangelista <r.evangelista@arcanys.com>
2017-02-08 18:18:16 +0800

    Swap system upgrade and sudoer file update.

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -8,7 +8,6 @@ apt -y install libffi-dev python-setuptools python-dev
 easy_install pip
 
 # Install Ansible repository.
-apt -y update && apt-get -y upgrade
 apt -y install software-properties-common
 apt-add-repository ppa:ansible/ansible
 
--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -3,6 +3,8 @@ set -e
 set -u
 set -x
 
+apt -y update && apt-get -y upgrade
+
 # Add vagrant user to sudoers.
 echo "vagrant ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
